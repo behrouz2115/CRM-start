d3ca7db91052ba660fdfdea1fead3654d641bc8a
Behrouz Rozmeh <behrouz.rozmeh@gmail.com>
2023-04-10 16:59:15 +0900

    items list updated

--- components/modules/ItemList.js
+++ components/modules/ItemList.js
@@ -1,18 +1,33 @@
-const ItemList = ({ form, setForm }) => {
-    const { products } = form;
+import FormInput from "./FormInput";
 
-    const addHandler = () => {
-        setForm({
-            ...form, products: [...products, { name: "", price: '', qty: "" }]
-        });
-        console.log(products);
-    }
-    return (
-        <div className="item-list">
-            <p>Purchased products</p>
-            <button onClick={addHandler}>Add Item</button>
+const ItemList = ({ form, setForm }) => {
+  const { products } = form;
+
+  const addHandler = () => {
+    setForm({
+      ...form,
+      products: [...products, { name: "", price: "", qty: "" }],
+    });
+    console.log(products);
+  };
+  const changeHandler = () => {};
+  return (
+    <div className="item-list">
+      <p>Purchased products</p>
+      {products.map((product) => (
+        <div className="form-input__list">
+          <FormInput
+            name="name"
+            lable="Product name"
+            type="text"
+            value={product.name}
+            onChange={changeHandler}
+          />
         </div>
-    );
+      ))}
+      <button onClick={addHandler}>Add Item</button>
+    </div>
+  );
 };
 
 export default ItemList;
