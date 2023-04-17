c4c7255eafee1ae914d257dba9b6f8bbd6454990
Behrouz Rozmeh <behrouz.rozmeh@gmail.com>
2023-04-17 20:27:42 +0900

    getting customer form db

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,8 @@ import styles from '@/styles/Home.module.css'
 import connecDB from '@/utils/connectDB'
 
 
-export default function Home() {
+export default function Index({ customer }) {
+  console.log(customer);
   return (
     <div>
       <h3>
@@ -13,10 +14,16 @@ export default function Home() {
     </div>
   )
 }
+
 export async function getServerSideProps() {
   try {
     await connecDB();
     const customer = await Customer.find();
+    return {
+      props: {
+        customer:JSON.parse(JSON.stringify(customer))
+      }
+    }
   } catch (err) {
     return {
       notFound:true,
