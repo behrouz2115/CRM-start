76df69e635eb9b4e9f0808e248ffe50e8d7b2f3d
Behrouz Rozmeh <behrouz.rozmeh@gmail.com>
2023-04-18 20:04:05 +0900

    backend detlete butume

---
+++ pages/api/delete/[customerId].js
@@ -0,0 +1,14 @@
+import connecDB from "@/utils/connectDB";
+
+export default async function handler(req, res) {
+    try {
+        await connecDB();
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ status: 'failed', massege: 'Error in connecting to DB' });
+        return;
+    }
+    if (req.method === "DELETE") {
+        
+    }
+}
