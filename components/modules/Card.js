c8a6da23782ec33f988ff53a189b0a80a325b97e
Behrouz Rozmeh <behrouz.rozmeh@gmail.com>
2023-04-18 19:56:03 +0900

    card updated

--- components/modules/Card.js
+++ components/modules/Card.js
@@ -1,9 +1,21 @@
+import Link from 'next/link';
 import React from 'react';
 
-const Card = () => {
+const Card = ({ customer }) => {
+    const deleteHandelr = () => {
+        
+    }
     return (
-        <div>
-            
+        <div className='card'>
+            <div className='card__details'>
+                <p>{customer.name} { customer.lastName }</p>
+                <p>{customer.email}</p>
+            </div>
+            <div className='card__buttons'>
+                <button onClick={deleteHandelr}>Delete</button>
+                <Link href={`/edit/${customer._id}`}>Edit</Link>
+                <Link href={`/customer/${customer._id}`} >Details</Link>
+            </div>
         </div>
     );
 };
