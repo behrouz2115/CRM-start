c39c606d9770f820cb3a7841d9a302c85806edc8
Behrouz Rozmeh <behrouz.rozmeh@gmail.com>
2023-04-19 21:16:50 +0900

    edit backend

--- pages/api/edit/[customerId].js
+++ pages/api/edit/[customerId].js
@@ -1,3 +1,4 @@
+import Customer from "@/models/Customer";
 import connecDB from "@/utils/connectDB";
 
 export default async function handler(req, res) {
@@ -7,5 +8,24 @@ export default async function handler(req, res) {
         console.log(err);
         res.status(500).json({ status: 'failed', massege: 'Error in connecting to DB' });
         return;
+    }
+    if (req.method === "PATCH") {
+        const id = req.query.customerId;
+        const data = req.bidy.data;
+        try {
+            const customer = await Customer.findOne({ _id: id });
+            customer.name = data.name;
+            customer.lastName = data.lastName;
+            customer.email = data.email;
+            customer.phone = data.phone;
+            customer.address = data.address;
+            customer.postalCode = data.postalCode;
+            customer.date = data.date;
+            customer.products = data.products;
+            customer.updatedAd = Date.now();
+        } catch (err) {
+            console.log(err);
+            res.status(400).json({status:'failed', message:"Error in editting data form db"})
+        }
     }
 }
