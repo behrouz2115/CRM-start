d5b303382ae314c1ee90906e63f6d427d6e3797d
Behrouz Rozmeh <behrouz.rozmeh@gmail.com>
2023-04-23 19:05:32 +0900

    ui edit

--- components/template/CustomerEditPage.js
+++ components/template/CustomerEditPage.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Form from '../modules/Form';
 
 const CustomerEditPage = ({ data, id }) => {
-    console.log({data,id});
+    const [form, setForm] = useState({
+        name: data.name,
+        lastName: data.lastName,
+        email: data.email,
+        phone: data.phone || "",
+        address: data.address || "",
+        phone: data.phone || "",
+        phone: data.phone || "",
+        postalCode : data.postalCode || "", 
+        date : data.date || "",
+        products : data.products
+    })
     return (
-        <div>
-            customer edit page
+        <div className='customer-page'>
+            <h4>Edded cusotmer</h4>
+            <Form form={form} setForm={setForm} />
         </div>
     );
 };
